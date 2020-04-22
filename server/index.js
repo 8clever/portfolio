a1049a160f2bac9adaac7e8344711535bfe6df92
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-04-23 01:53:00 +0300

    remove spdy

--- server/index.js
+++ server/index.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const app = express();
 const { cfg } = require("../config");
-const spdy = require("spdy");
 const fs = require("fs");
-
-const options = {
-    key: fs.readFileSync("./server.key"),
-    cert: fs.readFileSync("./server.cert")
-};
 
 (async () => {
     const webRouter = await require("../web").init();
     app.use(webRouter);
 
-    spdy.createServer(options, app).listen(cfg.app.port, () => {
+    app.listen(cfg.app.port, () => {
         console.log("server listening port: " + cfg.app.port);
     });
 })().catch(console.log);
