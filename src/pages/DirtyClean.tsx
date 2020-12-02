dc7c335c3591e0db5706858c8e99ef9b044916cd
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-12-02 13:50:37 +0300

    change dirty clean page

--- src/pages/DirtyClean.tsx
+++ src/pages/DirtyClean.tsx
@@ -1,5 +1,6 @@
 import { Button } from "reactstrap";
-import bg from "../images/dirty-clean-bg.png";
+import bg from "../images/back2-min.jpg";
+import mobile from "../images/mobile-dirtyclean.png";
 
 export const DirtyClean = () => {
   return (
@@ -9,36 +10,47 @@ export const DirtyClean = () => {
       height: "100vh",
       display: "flex",
       flexDirection: "column",
-      placeContent: "center"
+      placeContent: "center",
+      alignContent: "center"
     }}>
       <div style={{
-        padding: "40px",
-        background: "rgba(0,0,0,0.5)"
+        background: `url("${mobile}") content-box center / cover no-repeat`,
+        height: "90vh",
+        width: "45vh",
+        display: "flex",
+        flexDirection: "column-reverse",
       }}>
-        <div 
-          className="mb-4"
-          style={{ textAlign: "center", color: "white" }}>
-          <h1>Download Now</h1>
-        </div>
-        <div style={{ textAlign: "center" }}>
-          <a 
-            className="mr-4"
-            href="https://apps.apple.com/ru/app/dirty-clean/id1541008540">
-            <Button 
-              size="lg"
-              color="info">
-              <i className="fab fa-apple mr-2" />
-              iOS
-            </Button>
-          </a>
-          <a href="https://play.google.com/store/apps/details?id=com.VIPSoftware.DirtyClean&hl=ru&gl=US">
-            <Button 
-              size="lg"
-              color="info">
-              <i className="fab fa-android mr-2" />
-              Android
-            </Button>
-          </a>
+        <div style={{
+          margin: 25,
+          borderRadius: "0 0 50px 50px",
+          padding: "40px",
+          background: "rgba(0,0,0,0.5)",
+        }}>
+          <div 
+            className="mb-4"
+            style={{ textAlign: "center", color: "white" }}>
+            <h1>Download Now</h1>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <a 
+              className="mr-4"
+              href="https://apps.apple.com/ru/app/dirty-clean/id1541008540">
+              <Button 
+                size="lg"
+                color="info">
+                <i className="fab fa-apple mr-2" />
+                iOS
+              </Button>
+            </a>
+            <a href="https://play.google.com/store/apps/details?id=com.VIPSoftware.DirtyClean&hl=ru&gl=US">
+              <Button 
+                size="lg"
+                color="info">
+                <i className="fab fa-android mr-2" />
+                Android
+              </Button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
