302f46b2abb87938868b833994d5b263dd4b85a6
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-12-31 13:20:31 +0300

    change dirtyclean layout

--- src/pages/DirtyClean.tsx
+++ src/pages/DirtyClean.tsx
@@ -1,5 +1,6 @@
-import { Button } from "reactstrap";
+import { Button, Row, Col } from "reactstrap";
 import bg from "../images/back2-min.jpg";
+import React from "react";
 import mobile from "../images/mobile-dirtyclean.png";
 
 const Buttons = (props: {
@@ -34,41 +35,66 @@ const Buttons = (props: {
 }
 
 export const DirtyClean = () => {
+
   return (
-    <div style={{
-      background: `url("${bg}") content-box center / cover no-repeat`,
-      width: "100vw",
-      height: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      placeContent: "center",
-      alignItems: "center"
-    }}>
-      <div style={{
-        background: `url("${mobile}") content-box center / cover no-repeat`,
-        height: "90vh",
-        width: "45vh",
-        display: "flex",
-        flexDirection: "column-reverse",
-      }}>
-        <div style={{
-          margin: "2.4vh",
-          borderRadius: "0 0 2rem 2rem",
-          padding: "1rem",
-          background: "rgba(0,0,0,0.5)",
+    <Row noGutters>
+      <Col 
+        sm={12}
+        lg={6}
+        className="d-flex"
+        style={{ 
+          background: `url("${bg}") content-box center / cover no-repeat`,
+          height: "100vh" 
         }}>
-          <div style={{ 
-            marginBottom: "2rem",
-            textAlign: "center", 
-            color: "white" 
-          }}>
-            <h1>Download Now</h1>
-          </div>
-          <div style={{ textAlign: "center" }}>
-            <Buttons />
-          </div>
+        <img 
+          style={{
+            height: "95vh"
+          }}
+          className='img-fluid m-auto'
+          alt="Dirty Clean"
+          src={mobile} 
+        />
+      </Col>
+      <Col
+        style={{
+          background: "#212121",
+          height: "100vh"
+        }}
+        className="d-flex" 
+        sm={12}
+        lg={6}>
+        <div 
+          style={{
+            color: "whitesmoke"
+          }}
+          className="p-5 m-auto">
+          <h1>Dirty Clean</h1>
+          <hr 
+            style={{
+              borderColor: "whitesmoke"
+            }}
+          />
+          <h5>
+            May the broom be with you!
+          </h5>
+          <br />
+          <h5>
+            Logic Match2 puzzle game where the player has to learn all the delights of general cleaning and little more. Litter everything up with rubbish or be a brave connoisseur of cleanliness. Only you decide the fate of small areas of the city. Where each character has their own purpose and fear.
+          </h5>
+          <br />
+          <h5>
+            Explore different branches of improvements. Reveal all the chains of character interactions. Complete all missions and levels. Learn Zen in dirty cleaning. May the broom be with you!
+          </h5>
+          <br />
+          <h1>Download Now</h1>
+          <hr 
+            style={{
+              borderColor: "whitesmoke"
+            }}
+          />
+          <Buttons />
         </div>
-      </div>
-    </div>
+      </Col>
+    </Row>
   )
 }
