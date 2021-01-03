7be21b7eb0896c0280e7ea6e69d392ee483f8fbf
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2021-01-03 12:30:43 +0300

    remove inline css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { langStore } from '../src/store/lang';
-import { readFileSync } from "fs";
-import { join } from "path";
-
-declare type DocumentFiles = {
-  sharedFiles: readonly string[];
-  pageFiles: readonly string[];
-  allFiles: readonly string[];
-};
-
-class CustomHead extends Head {
-  getCssLinks (doc: DocumentFiles) {
-    const { assetPrefix } = this.context;
-    const files = doc.allFiles;
-    if (!files || files.length === 0) return null;
-
-    return files.filter(file => /\.css$/.test(file)).map(file => (
-      <style
-        key={file}
-        data-href={`${assetPrefix}/_next/${file}`}
-        dangerouslySetInnerHTML={{
-          __html: readFileSync(join(process.cwd(), '.build', file), 'utf-8'),
-        }}
-      />
-    )) as JSX.Element[];
-  }
-}
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -36,7 +10,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={langStore.lang}>
-        <CustomHead />
+        <Head />
         <body>
           <Main />
           <NextScript />
