6851a7d1a70d88a123667c6e34904dd8bf0b58c7
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2021-01-03 21:06:35 +0300

    add small fix

--- pages/dirtyclean.tsx
+++ pages/dirtyclean.tsx
@@ -55,6 +55,7 @@ export const DirtyClean = () => {
             width: "calc(90vh/2)"
           }}>
           <Image
+            alt="Dirty Clean mobile preview"
             src={mobile}
             layout="fill"
           />
